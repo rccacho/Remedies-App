850e86f0f40a55ef8bbbdc794e6bc2488e39705b
rccacho <rccacho@gmail.com>
2017-02-27 15:26:43 -0800

    data-bind illness array to make accessible to view

--- public/scripts/controllers/IllnessIndexController.js
+++ public/scripts/controllers/IllnessIndexController.js
@@ -2,17 +2,7 @@ angular
   .module('remedies')
   .controller('IllnessIndexController', IllnessIndexController);
 
-IllnessIndexController.$inject = [ '$http' ];
-
-function IllnessIndexController ($http) {
+function IllnessIndexController() {
   var vm = this;
-  $http({
-    method: 'GET',
-    url: ''
-  }).then(function successCallback(response) {
-  	console.log(response);
-  	vm.illnesses = response.data.illnesses;
-  }, function errorCallback(response) {
-    console.log('There was an error posting the data', response);
-  });
+  vm.illnesses = illness;
 }
