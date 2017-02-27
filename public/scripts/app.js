4badd44ac0dd92e181a74f2bb1ed2d10ac667c8f
rccacho <rccacho@gmail.com>
2017-02-27 11:39:34 -0800

    set up home page

---
+++ public/scripts/app.js
@@ -0,0 +1,27 @@
+console.log("app.js is working!")
+angular
+  .module('remedies', ['ngRoute'])
+  .config(config);
+
+config.$inject = ['$routeProvider', '$locationProvider'];
+
+function config ($routeProvider, $locationProvider) {
+
+  $routeProvider
+    .when('/', {
+      templateUrl: '/templates/illness-index.html',
+      controllerAs: 'illnessIndexController',
+      controller: 'IllnessIndexController'
+    })
+    // .when('/:id', {
+    //   templateUrl: '/templates/illness-show.html',
+    //   controllerAs: 'illnessShowController',
+    //   controller: 'IllnessShowController'
+    // })
+    .otherwise({ redirectTo: '/'})
+
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+  });
+}
